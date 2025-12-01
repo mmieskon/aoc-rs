a8b7d27afff1f1b2adea74c44d05cf34c9dc594c
mmieskon <105143729+mmieskon@users.noreply.github.com>
2025-12-01 20:39:52 +0200

    Add solution for day1 part2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     let path: &str = &std::env::args().nth(1).unwrap();
     let data = std::fs::read_to_string(path).unwrap();
 
-    let password = day1::calculate_password(&data);
+    let (passwd_part1, passwd_part2) = day1::calculate_password(&data);
     println!("Day1");
-    println!("  - Part1: {password}");
+    println!("  - Part1: {passwd_part1}");
+    println!("  - Part2: {passwd_part2}");
 }
--- src/year2025/day1.rs
+++ src/year2025/day1.rs
@@ -1,47 +1,59 @@
-use std::fmt::Display;
-
-pub fn calculate_password(data: &str) -> u32 {
+pub fn calculate_password(data: &str) -> (u32, u32) {
     let lines = data.lines();
-
     let mut dial = Dial::default();
 
-    let mut password: u32 = 0;
+    let mut passwd_part1: u32 = 0;
     for line in lines {
         dial.rotate(line);
         if dial.value == 0 {
-            password += 1;
+            passwd_part1 += 1;
         }
     }
 
-    password
+    let passwd_part2 = dial.visited_zeros;
+    (passwd_part1, passwd_part2)
 }
 
+#[derive(Debug)]
 struct Dial {
     value: i32,
+    visited_zeros: u32,
 }
 
 impl Default for Dial {
     fn default() -> Self {
-        Self { value: 50 }
+        Self {
+            value: 50,
+            visited_zeros: 0,
+        }
     }
 }
 
 impl Dial {
     fn rotate(&mut self, s: &str) {
         let amount: i32 = s[1..].parse().unwrap();
+        assert!(amount > 0);
 
         if s.starts_with('L') {
-            self.value = (self.value - amount) % 100;
+            self.visited_zeros += amount as u32 / 100;
+            let remainder = amount % 100;
+            if (self.value != 0) && (remainder >= self.value) {
+                self.visited_zeros += 1;
+            }
+
+            self.value = 99 - self.value;
+            self.value = (self.value + amount) % 100;
+            self.value = 99 - self.value;
         } else if s.starts_with('R') {
+            self.visited_zeros += amount as u32 / 100;
+            let remainder = amount % 100;
+            if remainder > (99 - self.value) {
+                self.visited_zeros += 1;
+            }
+
             self.value = (self.value + amount) % 100;
         } else {
             panic!();
         }
-    }
-}
-
-impl Display for Dial {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.value)
     }
 }
