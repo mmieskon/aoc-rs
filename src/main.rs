4c9de839a9205e7a4560f9859417ecd6bd98411e
mmieskon <105143729+mmieskon@users.noreply.github.com>
2025-12-02 15:08:14 +0200

    Clean up code

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ fn main() {
     let args = Args::parse();
     let data = std::fs::read_to_string(&args.path).unwrap();
 
-    match args.day {
-        1 => println!("{}", day1::solve(&data)),
+    let solution = match args.day {
+        1 => day1::solve(&data),
         2..=12 => todo!(),
         _ => args::print_usage_and_exit(),
-    }
+    };
+
+    println!("{solution}");
 }
